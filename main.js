b39cb55dbcd718511d363a40aa530e741c0c527b
LoveParrot-mango <76878537+LoveParrot-mango@users.noreply.github.com>
2021-10-16 10:21:59 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+leftWristX =  0;
+leftWristY = 0;
+difference = 0;
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet has been Initialized!')
+}
+
+function draw() {
+    background('#969a97');
+
+        document.getElementById("size").innerHTML = "Font size of the text is = " + difference + "px";
+        fill('#F90093');
+        stroke('#F90093');
+        text('Announcement', 50, 100);
+        textSize(difference);
+}
+
+
+function gotPoses (results)
+{
+    if(results.length > 0) {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWristX + "difference = " + difference);
+    }
+}
